f4a55299b3188704ac58fbad22a0ffafbd30610a
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2023-03-04 20:07:49 +0600

    redux,firebae initial setup. Implemented signup and login. Persist loggedIn users info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import { RouterProvider } from "react-router-dom";
-import routes from "./routes/routes";
+import { RouterProvider } from 'react-router-dom';
+import routes from './routes/routes';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import auth from './firebase/firebase.config';
+import { useDispatch } from 'react-redux';
+import { setUser } from './features/auth/authSlice';
+
+// console.log(process.env);
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user);
+        dispatch(setUser(user.email));
+      }
+    });
+  }, []);
   return (
     <>
       <RouterProvider router={routes} />
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,20 +1,23 @@
-import React, { useEffect, useState } from "react";
-import loginImage from "../assets/login.svg";
-import { useForm, useWatch } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import loginImage from '../assets/login.svg';
+import { useForm, useWatch } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { createUser } from '../features/auth/authSlice';
 const Signup = () => {
   const { handleSubmit, register, reset, control } = useForm();
-  const password = useWatch({ control, name: "password" });
-  const confirmPassword = useWatch({ control, name: "confirmPassword" });
+  const password = useWatch({ control, name: 'password' });
+  const confirmPassword = useWatch({ control, name: 'confirmPassword' });
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [disabled, setDisabled] = useState(true);
 
   useEffect(() => {
     if (
       password !== undefined &&
-      password !== "" &&
+      password !== '' &&
       confirmPassword !== undefined &&
-      confirmPassword !== "" &&
+      confirmPassword !== '' &&
       password === confirmPassword
     ) {
       setDisabled(false);
@@ -24,56 +27,57 @@ const Signup = () => {
   }, [password, confirmPassword]);
 
   const onSubmit = (data) => {
-    console.log(data);
+    // console.log(data);
+    dispatch(createUser({ email: data.email, password: data.password }));
   };
 
   return (
-    <div className='flex h-screen items-center pt-14'>
-      <div className='w-1/2'>
-        <img src={loginImage} className='h-full w-full' alt='' />
+    <div className="flex h-screen items-center pt-14">
+      <div className="w-1/2">
+        <img src={loginImage} className="h-full w-full" alt="" />
       </div>
-      <div className='w-1/2 grid place-items-center'>
-        <div className='bg-[#FFFAF4] rounded-lg grid place-items-center p-10'>
-          <h1 className='mb-10 font-medium text-2xl'>Sign up</h1>
+      <div className="w-1/2 grid place-items-center">
+        <div className="bg-[#FFFAF4] rounded-lg grid place-items-center p-10">
+          <h1 className="mb-10 font-medium text-2xl">Sign up</h1>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className='space-y-3'>
-              <div className='flex flex-col items-start'>
-                <label htmlFor='email' className='ml-5'>
+            <div className="space-y-3">
+              <div className="flex flex-col items-start">
+                <label htmlFor="email" className="ml-5">
                   Email
                 </label>
                 <input
-                  type='email'
-                  name='email'
-                  id='email'
-                  {...register("email")}
+                  type="email"
+                  name="email"
+                  id="email"
+                  {...register('email')}
                 />
               </div>
 
-              <div className='flex flex-col items-start'>
-                <label htmlFor='password' className='ml-5'>
+              <div className="flex flex-col items-start">
+                <label htmlFor="password" className="ml-5">
                   Password
                 </label>
                 <input
-                  type='password'
-                  name='password'
-                  id='password'
-                  {...register("password")}
+                  type="password"
+                  name="password"
+                  id="password"
+                  {...register('password')}
                 />
               </div>
-              <div className='flex flex-col items-start'>
-                <label htmlFor='confirm-password' className='ml-5'>
+              <div className="flex flex-col items-start">
+                <label htmlFor="confirm-password" className="ml-5">
                   Confirm Password
                 </label>
                 <input
-                  type='password'
-                  id='confirm-password'
-                  {...register("confirmPassword")}
+                  type="password"
+                  id="confirm-password"
+                  {...register('confirmPassword')}
                 />
               </div>
-              <div className='!mt-8 '>
+              <div className="!mt-8 ">
                 <button
-                  type='submit'
-                  className='font-bold text-white py-3 rounded-full bg-primary w-full disabled:bg-gray-300 disabled:cursor-not-allowed'
+                  type="submit"
+                  className="font-bold text-white py-3 rounded-full bg-primary w-full disabled:bg-gray-300 disabled:cursor-not-allowed"
                   disabled={disabled}
                 >
                   Sign up
@@ -81,10 +85,10 @@ const Signup = () => {
               </div>
               <div>
                 <p>
-                  Already have an account?{" "}
+                  Already have an account?{' '}
                   <span
-                    className='text-primary hover:underline cursor-pointer'
-                    onClick={() => navigate("/login")}
+                    className="text-primary hover:underline cursor-pointer"
+                    onClick={() => navigate('/login')}
                   >
                     Login
                   </span>
